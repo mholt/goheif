568f135b27375f089294c34695d02f1e20b79393
Matthew Holt <mholt@users.noreply.github.com>
2023-02-27 22:27:39 -0700

    Fix imports

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module github.com/mholt/goheif
+
+go 1.20
+
+require github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd
--- goheif.go
+++ goheif.go
@@ -8,8 +8,8 @@ import (
 	"io"
 	"io/ioutil"
 
-	"github.com/adrium/goheif/heif"
-	"github.com/adrium/goheif/libde265"
+	"github.com/mholt/goheif/heif"
+	"github.com/mholt/goheif/libde265"
 )
 
 // SafeEncoding uses more memory but seems to make
